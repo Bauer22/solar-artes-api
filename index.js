d79e097cd99dde81be0b92d1686921218c41663b
Bauer22 <128606776+Bauer22@users.noreply.github.com>
2026-06-08 09:20:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+{
+  "name": "solar-artes-api",
+  "version": "1.0.0",
+  "description": "Solar Artes API - Backend MySQL",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mysql2": "^3.6.0",
+    "cors": "^2.8.5",
+    "dotenv": "^16.3.1"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  }
+}
